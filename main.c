c551f5cd6c9b2a0031d4ce1c75a8f7fb7fe18cc7
= <=>
2024-08-28 14:50:24 -0300

    Subtracao

--- main.c
+++ main.c
@@ -87,15 +87,86 @@ char *somaStr(char *strA, char *strB) {
     }
     return result;
 }
+
+char sub(char a, char b) {
+    if (a == '1' && b == '1') {
+        return '0';
+    } else if (a == '0' && b == '1') {
+        return '2';
+    } else if (a == '1' && b == '0') {
+        return '1';
+    } else {
+        return '0';
+    }
+}
+
+char *subStr(char *strA, char *strB) {
+    char borrow = '0';
+    int lenA = strlen(strA);
+    int lenB = strlen(strB);
+    if (lenA - lenB > 0) {
+        strB = shiftRight(strB, lenB, lenA - lenB);
+    } else if (lenB - lenA > 0) {
+        strA = shiftRight(strA, lenA, lenB - lenA);
+    }
+    int len = strlen(strA);
+    char *result = malloc(len);
+    result = memset(result, '0', len);
+    for (int i = len - 1; i >= 0; i--) {
+        char res = sub(strA[i], strB[i]);
+        switch (res) {
+            case '0':
+                if (borrow == '0') {
+                    result[i] = '0';
+                    borrow = '0';
+                } else {
+                    result[i] = '1';
+                    borrow = '1';
+                }
+                break;
+            case '1':
+                if (borrow == '0') {
+                    result[i] = '1';
+                    borrow = '0';
+                } else {
+                    result[i] = '0';
+                    borrow = '0';
+                }
+                break;
+            case '2':
+                if (borrow == '0') {
+                    result[i] = '1';
+                    borrow = '1';
+                } else {
+                    result[i] = '0';
+                    borrow = '1';
+                }
+                break;
+        }
+    }
+    return result;
+}
+
+char karatsuba(char *strA, char *strB, int len) {
+    
+
+}
+
 int main(int argc, char *argv[]){
 
     char *strA = argv[1];
     char *strB = argv[2];
     int len = strlen(strA);
     int len2 = strlen(strB);
+    int dif = len - len2;
+    if (dif > 0) {
+        strB = shiftRight(strB, len2, dif);
+    } else if (dif < 0) {
+        strA = shiftRight(strA, len, -dif);
+    }
     printf("String A: %s\n", strA);
     printf("String B: %s\n", strB);;
-    printf("Soma: %s\n", somaStr(strA, strB));
+    printf("Soma: %s\n", subStr(strA, strB));
     /** TO-DO: paramêtros 10110 e 1101101 quebraram o algoritmo da soma, preciso encontrar o problema e corrigir */
     return 0;
 
