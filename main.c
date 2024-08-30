70f230b84c862b1e7f36d098421c309fb735e509
= <=>
2024-08-30 15:57:48 -0300

    checkpoint: ainda nao funciona mas vou apgar e recomecar a funcao karatsuba

--- main.c
+++ main.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 char mult(char a, char b) {
     return (a == '1' && b == '1') ? '1' : '0'; // forçando o retorno manter em string
 }
 
 char *shift(char *str, int len, int shift, int left) {
-    // char *shifted = malloc(len + shift + 1);
-    // if (left) {
-    //     strcat(shifted, str);
-    //     memset(shifted + len, '0', shift);
-    // } else {
-    //     memset(shifted, '0', shift);
-    //     strcat(shifted, str);
-    // }
-    // return shifted;
-
     char *shifted = malloc(len + shift + 1);
     memset(shifted, '0', len + shift);
     shifted[len + shift] = '\0';
@@ -157,34 +148,91 @@ char *subStr(char *strA, char *strB) {
     return result;
 }
 
+void particiona(char **str, char **x1, char **x2) {
+    int len = strlen(*str);
+    if (len % 2 != 0) {
+        *str = shiftRight(*str, len, 1);
+        len += 1;
+    }
+    int meio = len / 2;
+    *x1 = malloc(meio + 1);
+    *x2 = malloc(meio +  1); 
+    strncpy(*x1, *str, meio);
+    strncpy(*x2, *str + meio, meio);
+    (*x1)[meio] = '\0';
+    (*x2)[meio] = '\0';
+}
 
-char *karatsuba(char *strA, char *strB, int len) {
+char *karatsuba(char *strA, char *strB, int *call) {
     int lenA = strlen(strA);
     int lenB = strlen(strB);
+    *call += 1;
+    char *tab = malloc(*call + 1);
+    tab=memset(tab, '\t', *call);
+    printf("%sRecebido: A = %s, B = %s\n", tab, strA, strB);
     if (lenA == 1 && lenB == 1) {
         char *result = malloc(1);
         result[0] = mult(strA[0], strB[0]);
         return result;
     }
-    char *resp = karatsuba(strA, strB, len / 2);
+    if (lenA != lenB) {
+        normalize(&strA, &strB);
+    }
+    int len = strlen(strA);
+    char *a1 = malloc(len / 2 + 1);
+    char *a2 = malloc(len / 2 + 1);
+    char *b1 = malloc(len / 2 + 1);
+    char *b2 = malloc(len / 2 + 1);
+    particiona(&strA, &a1, &a2);
+    particiona(&strB, &b1, &b2);
+    printf("%sa1: %s\n", tab, a1);
+    printf("%sa2: %s\n", tab, a2);
+    printf("%sb1: %s\n", tab, b1);
+    printf("%sb2: %s\n", tab, b2);
+    printf("%sChamando a1 * b1\n", tab);
+    char *p1 = karatsuba(a1, b1, call);
+    printf("%sp1: %s\n", tab, p1);
+    printf("%sChamando a2 * b2\n", tab);
+    char *p2 = karatsuba(a2, b2, call);
+    printf("%sp2: %s\n", tab, p2);
+    printf("%sChamando (a1 + a2) * (b1 + b2)\n", tab);
+    char *a1a2 = somaStr(a1, a2);
+    printf("%sa1a2: %s\n", tab, a1a2); // TO-DO: a1a2 está com problema (10110 e 1101101
+    char *b1b2 = somaStr(b1, b2);
+    printf("%sb1b2: %s\n", tab, b1b2);
+    char *p3 = karatsuba(a1a2, b1b2, call);
+    printf("%sp3: %s\n", tab, p3);
+    printf("%sCalculando p4\n", tab);
+    char *p4 = subStr(p3, somaStr(p1, p2));
+    printf("%sp4: %s\n", tab, p4);
+    char *result;
+    int m = floor(len / 2);
+    p1 = shiftLeft(p1, strlen(p1), 2*m);
+    printf("%sp1 shiftado: %s\n", tab, p1);
+    p4 = shiftLeft(p4, strlen(p4), m);
+    printf("%sp4 shiftado: %s\n", tab, p4);
+    result = somaStr(p1, p2);
+    printf("%sp1 + p2: %s\n", tab, result);
+    result = somaStr(result, p4);
+    printf("%sp1 + p2 + p4: %s\n", tab, result);
+    return result;
+
 }
 
 int main(int argc, char *argv[]){
 
     char *strA = argv[1];
     char *strB = argv[2];
-    // int len = strlen(strA);
-    // int len2 = strlen(strB);
-    // int dif = len - len2;
-    // if (dif > 0) {
-    //     strB = shiftRight(strB, len2, dif);
-    // } else if (dif < 0) {
-    //     strA = shiftRight(strA, len, -dif);
-    // }
+    int call = 0;
     normalize(&strA, &strB);
     printf("String A: %s\n", strA);
     printf("String B: %s\n", strB);
-    printf("Soma: %s\n", subStr(strA, strB));
+    printf("Soma: %s\n", karatsuba(strA, strB, &call));
+    // char *a1;
+    // char *a2;
+    // particiona(&strA, &a1, &a2);
+    // printf("a1: %s\n", a1);
+    // printf("a2: %s\n", a2);
     /** TO-DO: paramêtros 10110 e 1101101 quebraram o algoritmo da soma, preciso encontrar o problema e corrigir */
     return 0;
 
